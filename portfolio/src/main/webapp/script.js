4f78b1e9af6504171ca12b384dc6c64a96ae62d2
GROD8962 <47120200+GROD8962@users.noreply.github.com>
2021-06-29 23:15:08 -0400

    Week 3 MVP
    Implements a simple hardcoded chart for LEGO BIONICLE set distribution per year.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -29,8 +29,43 @@ function addRandomGreeting() {
 
 async function showString() {
   const responseFromServer = await fetch('/hello');
-  const textFromResponse = await responseFromServer.text();
+  const textFromResponse = await responseFromServer.json();
+
 
   const stringContainer = document.getElementById('string-container');
   stringContainer.innerText = textFromResponse;
+}
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+/** Creates a chart and adds it to the page. */
+function drawChart() {
+  const data = new google.visualization.DataTable();
+  data.addColumn('string', 'Animal');
+  data.addColumn('number', 'Count');
+        data.addRows([
+          ['2001', 23],
+          ['2002', 22],
+          ['2003', 23],
+          ['2004', 23],
+          ['2005', 28],
+          ['2006', 28],
+          ['2007', 25],
+          ['2008', 28],
+          ['2009', 27],
+          ['2010', 6],
+          ['2015', 18],
+          ['2016', 18]
+        ]);
+
+  const options = {
+    'title': 'Distribution of BIONICLE sets per year (2001-2010; 2015-2016)',
+    'width':600,
+    'height':600
+  };
+
+  const chart = new google.visualization.PieChart(
+      document.getElementById('chart-container'));
+  chart.draw(data, options);
 }
